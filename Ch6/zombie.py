8c5392611d8bb60584557d7745733dd9a7190227
Daniel Ye <48700147+daniel-yili-ye@users.noreply.github.com>
2020-03-23 19:05:42 -0400

    Add files via upload

--- Ch6/zombie.py
+++ Ch6/zombie.py
@@ -1,41 +1,88 @@
 import zombiedice
+import random
 
-class MyZombie:
+"""
+1. A bot that, after the first roll, randomly decides if it will continue or stop
+2. A bot that stops rolling after it has rolled two brains
+3. A bot that stops rolling after it has rolled two shotguns
+4. A bot that initially decides it’ll roll the dice one to four times, but will stop early if it rolls two shotguns
+5. A bot that stops rolling after it has rolled more shotguns than brains
+"""
+
+class MyZombie_1:
     def __init__(self, name):
-        # All zombies must have a name:
         self.name = name
-
     def turn(self, gameState):
-        # gameState is a dict with info about the current state of the game.
-        # You can choose to ignore it in your code.
-
-        diceRollResults = zombiedice.roll() # first roll
-        # roll() returns a dictionary with keys 'brains', 'shotgun', and
-        # 'footsteps' with how many rolls of each type there were.
-        # The 'rolls' key is a list of (color, icon) tuples with the
-        # exact roll result information.
-        # Example of a roll() return value:
-        # {'brains': 1, 'footsteps': 1, 'shotgun': 1,
-        #  'rolls': [('yellow', 'brains'), ('red', 'footsteps'),
-        #            ('green', 'shotgun')]}
-
-        # REPLACE THIS ZOMBIE CODE WITH YOUR OWN:
-        brains = 0
+        diceRollResults = zombiedice.roll()
         while diceRollResults is not None:
-            brains += diceRollResults['brains']
-
-            if brains < 2:
-                diceRollResults = zombiedice.roll() # roll again
+            r = (random.randint(0,1))
+            if r == 0:
+                diceRollResults = zombiedice.roll()
             else:
                 break
 
+class MyZombie_2:
+    def __init__(self, name):
+        self.name = name
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+        brains = 0
+        while diceRollResults is not None:
+            brains += diceRollResults['brains']
+            if brains < 2:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+class MyZombie_3:
+    def __init__(self, name):
+        self.name = name
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+        shotgun = 0
+        while diceRollResults is not None:
+            shotgun += diceRollResults['shotgun']
+            if shotgun < 2:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+class MyZombie_4:
+    def __init__(self, name):
+        self.name = name
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll() # first roll
+        shotgun = 0
+        while diceRollResults is not None:
+            shotgun += diceRollResults['shotgun']
+            for i in range(4):
+                if shotgun < 2:
+                    diceRollResults = zombiedice.roll() # roll again
+                else:
+                    break
+            break
+
+class MyZombie_5:
+    def __init__(self, name):
+        self.name = name
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll() # first roll
+        shotgun = 0
+        brains = 0
+        while diceRollResults is not None:
+            shotgun += diceRollResults['shotgun']
+            brains += diceRollResults['brains']
+            if shotgun > brains:
+                break # roll again
+            else:
+                diceRollResults = zombiedice.roll()
+
 zombies = (
-    zombiedice.examples.RandomCoinFlipZombie(name='Random'),
-    zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
-    MyZombie(name='My Zombie Bot')
-    # Add any other zombie players here.
+    MyZombie_1(name='Random'),
+    MyZombie_2(name='Stop at 2 Brains'),
+    MyZombie_3(name='Stop at 2 Shotguns'),
+    MyZombie_4(name='Roll 4 Times'),
+    MyZombie_5(name='Stop at SmB')
 )
 
 # Uncomment one of the following lines to run in CLI or Web GUI mode:
